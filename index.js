466de1287d39757a55380c321613f408d059aa28
honorific <honorific.info@gmail.com>
2024-11-01 22:54:10 +0330

    connecting to mongoose

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import modules from './modules/index.js'
+import modules from './modules/api/v1/index.js'
+import mongoose from 'mongoose'
 
 dotenv.config()
 const port = process.env.BEPORT
 
 const app = express()
+app.use(express.json())
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(() => console.log("Couldn't connect to MongoDB"))
 
 app.use(modules)
 
