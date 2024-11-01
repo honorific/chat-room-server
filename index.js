742927509bc0f44a42c8c0c5fe955e304a6b61b7
honorific <honorific.info@gmail.com>
2024-11-01 22:17:55 +0330

    setup api module

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import modules from './modules/index.js'
 
 dotenv.config()
 const port = process.env.BEPORT
 
 const app = express()
+
+app.use(modules)
 
 app.listen(port, () => {
   console.log(`Backend running in port ${port}`)
