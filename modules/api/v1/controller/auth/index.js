39910de9786d336f51a42806b1683bbade9be6ae
honorific <honorific.info@gmail.com>
2024-11-02 20:52:51 +0330

    return to register controller and user model before testing dynamic models and collections.

--- modules/api/v1/controller/auth/index.js
+++ modules/api/v1/controller/auth/index.js
@@ -1,16 +1,12 @@
-import mongoose from 'mongoose'
 import User from '../../models/User.js'
 
 export const register = async (req, res) => {
   try {
-    const newUser = mongoose.model('User_' + String(Date.now()), User)
+    const newUser = new User({
+      username: req.body.username,
+    })
 
-    // new User({
-    //   username: req.body.username,
-    // })
-    const final = new newUser({username: req.body.username})
-
-    const user = await final.save()
+    const user = await newUser.save()
     res.status(201).json(user)
   } catch (err) {
     console.log(err)
--- modules/api/v1/models/User.js
+++ modules/api/v1/models/User.js
@@ -7,4 +7,4 @@ const UserSchema = new mongoose.Schema(
   {timestamps: true},
 )
 
-export default UserSchema
+export default mongoose.model('User', UserSchema)
