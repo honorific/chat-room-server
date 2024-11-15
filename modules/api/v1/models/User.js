1e7edb551c554c7b9a1e99426b129cf4f8f8786d
honorific <honorific.info@gmail.com>
2024-11-15 22:32:19 +0330

    add gender to user model

--- modules/api/v1/models/User.js
+++ modules/api/v1/models/User.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const UserSchema = new mongoose.Schema(
   {
     username: {type: String, required: true, min: 3, max: 20, unique: true},
+    gender: {type: String, required: true, enum: ['male', 'female']},
   },
   {timestamps: true},
 )
