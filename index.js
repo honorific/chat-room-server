48bf10fe733543d4906244d1a9ee880666b44e57
honorific <honorific.info@gmail.com>
2024-11-15 22:31:53 +0330

    set headers as middleware for each request

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const port = process.env.BEPORT
 
 const app = express()
 app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, Content-Type, Authorization',
+  )
+  next()
+})
+
 
 mongoose
   .connect(process.env.MONGO_URL)
