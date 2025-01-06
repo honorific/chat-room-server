6989b7f18e56dedf3d6020e59a68390e49623ea4
honorific <honorific.info@gmail.com>
2025-01-06 22:01:36 +0330

    chat model

---
+++ modules/api/v1/models/Chat.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const messagesSchema = new mongoose.Schema({
+  sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  receiver: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  dateTime: {type: Date, required: true},
+  msg: {type: String, required: true},
+})
+
+const ChatSchema = new mongoose.Schema({
+  room: {type: String, required: true, min: 1, max: 20, unique: true},
+  messages: [messagesSchema],
+})
+
+export default mongoose.model('Chat', ChatSchema)
