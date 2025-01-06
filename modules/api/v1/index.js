4bf4035eaead0cdffd0dd68c3db25d33f48a1fae
honorific <honorific.info@gmail.com>
2025-01-06 22:28:02 +0330

    chat route

--- modules/api/v1/index.js
+++ modules/api/v1/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import authRouter from './routes/auth.js'
 import userRouter from './routes/user.js'
+import chatRouter from './routes/chat.js'
 
 const router = express.Router()
 
 router.use('/api/v1/auth', authRouter)
 router.use('/api/v1/user', userRouter)
+router.use('/api/v1/chat', chatRouter)
 
 export default router
