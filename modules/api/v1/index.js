6ceeb92b449b56bb726cdc5bda9e39f1a2605f18
honorific <honorific.info@gmail.com>
2025-01-22 00:22:54 +0330

    add token route to api routes

--- modules/api/v1/index.js
+++ modules/api/v1/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import authRouter from './routes/auth.js'
 import userRouter from './routes/user.js'
 import chatRouter from './routes/chat.js'
+import tokenRouter from './routes/token.js'
 
 const router = express.Router()
 
 router.use('/api/v1/auth', authRouter)
 router.use('/api/v1/user', userRouter)
 router.use('/api/v1/chat', chatRouter)
+router.use('/api/v1/token', tokenRouter)
 
 export default router
