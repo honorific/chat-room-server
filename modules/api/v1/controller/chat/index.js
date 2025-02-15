bfee2c2dc88f21b0cd8259bd331a0096e24a8ea5
honorific <honorific.info@gmail.com>
2025-02-16 00:25:58 +0330

    append the message to the room if already that room was in chats document in db

--- modules/api/v1/controller/chat/index.js
+++ modules/api/v1/controller/chat/index.js
@@ -1,27 +1,55 @@
 import Chat from '../../models/Chat.js'
 
 export const sendMessage = async (req, res) => {
-  try {
-    const newChat = new Chat({
-      room: req.body.room,
-      messages: [
+  const messages = {
+    sender: req.body.sender,
+    receiver: req.body.receiver,
+    dateTime: req.body.dateTime,
+    msg: req.body.msg,
+  }
+  const room = await checkRoomInDb(req.body.room)
+  console.log('room is: ', room)
+  if (room) {
+    try {
+      const newMessage = await Chat.updateOne(
+        {room: req.body.room},
         {
-          sender: req.body.sender,
-          receiver: req.body.receiver,
-          dateTime: req.body.dateTime,
-          msg: req.body.msg,
+          $push: {
+            messages,
+          },
         },
-      ],
-    })
-
-    const chat = await newChat.save()
-    if (chat) {
-      res.status(201).json(chat)
-    } else {
-      res.status(400).json('could not send the message')
+      )
+      res.status(201).json({success: true, newMessage})
+    } catch (err) {
+      console.log(err)
+      res.status(500).json('could not insert new message')
     }
+  } else {
+    try {
+      const newChat = new Chat({
+        room: req.body.room,
+        messages: [messages],
+      })
+
+      const chat = await newChat.save()
+      if (chat) {
+        res.status(201).json(chat)
+      } else {
+        res.status(400).json('could not send the message')
+      }
+    } catch (err) {
+      console.log(err)
+      res.status(500).json('message server error')
+    }
+  }
+}
+
+export const checkRoomInDb = async (room) => {
+  try {
+    const roomInDb = await Chat.findOne({room})
+    if (roomInDb) return true
   } catch (err) {
     console.log(err)
-    res.status(500).json('message server error')
+    return false
   }
 }
