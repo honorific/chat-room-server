782045073695e7d7d810c0a83e73a920167290a5
honorific <honorific.info@gmail.com>
2025-02-16 00:24:27 +0330

    creating a new accessToken if accessToken verified.

--- modules/api/v1/middlewares/auth/index.js
+++ modules/api/v1/middlewares/auth/index.js
@@ -1,19 +1,33 @@
 import jwt from 'jsonwebtoken'
+import {generateAccessToken} from '../../controller/token/generateAccessToken.js'
 
 export const verifyAccessToken = async (req, res, next) => {
-  jwt.verify(
-    req.body.token ?? req.query.token,
-    process.env.ACCESS_TOKEN_SECRET,
-    (error, _verified) => {
-      if (error) {
-        return res
-          .status(401)
-          .json({success: false, msg: 'unAuthoried in middleware'})
-      } else {
-        next()
-      }
-    },
-  )
+  const tokenInRequest = req.body.token ?? req.query.token
+  const {
+    token: {refreshToken},
+  } = jwt.decode(tokenInRequest)
+  console.log('token is in verifyAccessToken: ', tokenInRequest)
+  if (refreshToken) {
+    jwt.verify(
+      tokenInRequest,
+      process.env.ACCESS_TOKEN_SECRET,
+      (error, _verified) => {
+        if (error) {
+          try {
+            generateAccessToken(refreshToken)
+            next()
+          } catch (err) {
+            console.log(err)
+            res.status(500).json('error while creating token')
+          }
+        } else {
+          next()
+        }
+      },
+    )
+  } else {
+    res.status(401).json('unauthorized in middleware')
+  }
 }
 
 export const getRefreshTokenFromAccessToken = (req, res, next) => {
