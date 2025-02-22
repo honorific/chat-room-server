88247da4b8ad581449d685e9e5fea14867cad392
honorific <honorific.info@gmail.com>
2025-02-23 00:16:56 +0330

    edit chat model

--- modules/api/v1/models/Chat.js
+++ modules/api/v1/models/Chat.js
@@ -2,14 +2,25 @@ import mongoose from 'mongoose'
 
 const messagesSchema = new mongoose.Schema({
   sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-  conversationId: {type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true},
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   dateTime: {type: Date, required: true},
   msg: {type: String, required: true},
 })
 
-const ChatSchema = new mongoose.Schema({
-  room: {type: String, required: true, min: 1, max: 20, unique: true},
-  messages: [messagesSchema],
-})
+const ChatSchema = new mongoose.Schema(
+  {
+    conversation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Conversation',
+      required: true,
+    },
+    messages: [messagesSchema],
+  },
+  {timestamps: true},
+)
 
 export default mongoose.model('Chat', ChatSchema)
