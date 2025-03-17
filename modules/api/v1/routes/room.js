980a4075c8f23d94f79876d47551cdb7eb11107f
honorific <honorific.info@gmail.com>
2025-03-17 14:24:16 +0330

    add verifyAccessToken middleware to createRoom route

--- modules/api/v1/routes/room.js
+++ modules/api/v1/routes/room.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import {createRoom} from '../controller/room/index.js'
+import { verifyAccessToken } from '../middlewares/auth/index.js'
 
 const router = express.Router()
 
-router.post('/room', createRoom)
+router.post('/room', verifyAccessToken, createRoom)
 
 export default router
