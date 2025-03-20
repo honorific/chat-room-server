afc25f1493374ae429db8fd775ec7fc2715a7211
honorific <honorific.info@gmail.com>
2025-03-20 23:28:40 +0330

    create new room route

--- modules/api/v1/index.js
+++ modules/api/v1/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+
 import authRouter from './routes/auth.js'
 import userRouter from './routes/user.js'
 import chatRouter from './routes/chat.js'
 import tokenRouter from './routes/token.js'
+import roomRouter from './routes/room.js'
 
 const router = express.Router()
 
@@ -10,5 +12,6 @@ router.use('/api/v1/auth', authRouter)
 router.use('/api/v1/user', userRouter)
 router.use('/api/v1/chat', chatRouter)
 router.use('/api/v1/token', tokenRouter)
+router.use('/api/v1/room', roomRouter)
 
 export default router
