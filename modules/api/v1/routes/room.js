564545008c001924ce71a175b7e82ab37039fdd8
honorific <honorific.info@gmail.com>
2025-03-24 23:18:02 +0330

    deleteRoom route

--- modules/api/v1/routes/room.js
+++ modules/api/v1/routes/room.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {createRoom} from '../controller/room/index.js'
-import { verifyAccessToken } from '../middlewares/auth/index.js'
+import {createRoom, deleteRoom} from '../controller/room/index.js'
+import {verifyAccessToken} from '../middlewares/auth/index.js'
 
 const router = express.Router()
 
 router.post('/create', verifyAccessToken, createRoom)
+router.delete('/delete', verifyAccessToken, deleteRoom)
 
 export default router
