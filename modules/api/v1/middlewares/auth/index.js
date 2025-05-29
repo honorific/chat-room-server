33a9a7bc0a6e8f9fb3db23f62e35d07c263c8095
honorific <honorific.info@gmail.com>
2025-05-29 22:13:52 +0330

    fix verifyAccessToken

--- modules/api/v1/middlewares/auth/index.js
+++ modules/api/v1/middlewares/auth/index.js
@@ -1,37 +1,50 @@
 import jwt from 'jsonwebtoken'
 import {generateAccessToken} from '../../controller/token/generateAccessToken.js'
 import getTokenInRequest from '../../controller/token/getTokenInRequest.js'
+import {findRefreshTokenByTokenId} from '../../controller/token/findRefreshTokenByTokenId.js'
 
 export const verifyAccessToken = async (req, res, next) => {
   const tokenInReq = getTokenInRequest(req.headers.authorization)
   const {
-    token: {refreshToken},
+    token: {tokenId},
   } = jwt.decode(tokenInReq)
-  console.log('token is in verifyAccessToken: ', authHeader)
-  // get refresh token in correct way,
-  // have to check does refresh token is expired or not.
-  // it is correct to create a new access token if refresh token does not expired
-  // as long as user has valid refresh token, can access.
-  if (refreshToken) {
-    jwt.verify(
-      tokenInReq,
-      process.env.ACCESS_TOKEN_SECRET,
-      (error, _verified) => {
-        if (error) {
-          try {
-            generateAccessToken(refreshToken)
-            next()
-          } catch (err) {
-            console.log(err)
-            res.status(500).json('error while creating token')
+  try {
+    const refreshTokenInDb = await findRefreshTokenByTokenId(tokenInReq)
+    console.log(refreshTokenInDb)
+    if (refreshTokenInDb.success) {
+      jwt.verify(
+        refreshTokenInDb,
+        process.env.REFRESH_TOKEN_SECRET,
+        (refreshTokenError, _verified) => {
+          if (refreshTokenError) {
+            res.status(401).json('unAuthorized')
+          } else {
+            jwt.verify(
+              tokenInReq,
+              process.env.ACCESS_TOKEN_SECRET,
+              (accessTokenError, _verified) => {
+                if (accessTokenError) {
+                  try {
+                    generateAccessToken(tokenId)
+                    next()
+                  } catch (err) {
+                    console.log(err)
+                    res.status(500).json('error while creating token')
+                  }
+                } else {
+                  next()
+                }
+              },
+            )
           }
-        } else {
-          next()
-        }
-      },
-    )
-  } else {
-    res.status(401).json('unauthorized in middleware')
+        },
+      )
+    } else {
+      res.status(500).json(refreshTokenInDb.msg)
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json('error while retrieving refresh token')
   }
 }
 
