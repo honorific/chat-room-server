79c88e16f12396240219a7c997a652703dc0f61f
honorific <honorific.info@gmail.com>
2025-07-02 23:53:16 +0330

    change the model of tokenSchema

--- modules/api/v1/models/Token.js
+++ modules/api/v1/models/Token.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const TokenSchema = new mongoose.Schema(
   {
     refreshToken: {type: String, required: true, unique: true},
+    tokenId: {type: String, required: true, unique: true},
   },
   {timestamps: true},
 )
